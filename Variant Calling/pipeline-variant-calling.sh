3fc2dad703a97538044988fbbc42ceb9445a66eb
mrivarauy <57630161+mrivarauy@users.noreply.github.com>
2022-03-03 16:07:01 -0300

    Update pipeline-variant-calling.sh

--- Variant Calling/pipeline-variant-calling.sh
+++ Variant Calling/pipeline-variant-calling.sh
@@ -2,7 +2,7 @@
 BASE="/path/to/base/directory/variant-calling" #path to base drirectory
 
 # Set up input data
-REF_DIR="/media/Disco9/datos_DeepVariant/ref_dir" #reference directory (it contains human genome GRCh38 reference file and reference vcf file)
+REF_DIR="/path/to/ref_dir" #reference directory (it contains human genome GRCh38 reference file and reference vcf file)
 INPUT_DIR="${BASE}/input" # input directory (it contains fastq files and bam files of reads mapped to reference)
 REF="GCA_000001405.15_GRCh38_no_alt_analysis_set.fna" #human genome GRCh38 reference file
 
